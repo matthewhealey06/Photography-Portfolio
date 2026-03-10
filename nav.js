a4d3f7943b4c37e661907c9b579b87c07245f182
Matthew Healey <matthewhealey06@gmail.com>
2026-03-10 21:19:03 +0000

    Fix nav state JS

--- nav.js
+++ nav.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     const currentPath = window.location.pathname.toLowerCase();
-    const currentFile = currentPath.split('/').pop() || 'index.html';
+    const currentFile = currentPath.split('/').pop().replace(".html", "") || 'index' 
     const links = document.querySelectorAll('nav a');
 
     links.forEach(link => {
         link.classList.remove('active');
         const linkHref = link.getAttribute('href').toLowerCase();
-        const linkFile = linkHref.split('/').pop();
+        const linkFile = linkHref.split('/').pop().replace(".html", "");
 
         if (
             linkFile === currentFile ||
-            (currentFile === '' && linkFile === 'index.html') ||
-            (currentFile === 'index.html' && linkFile === '')
+            (currentFile === '' && linkFile === 'index') ||
+            (currentFile === 'index' && linkFile === '')
         ) {
             link.classList.add('active');
         }
